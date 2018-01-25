54b445241e14d9af2f95b358004278c1a0a4208d
HuangZhiHao <498641239@qq.com>
2018-01-25 19:39:28 +0800

    完整版本

--- src/main/java/com/helperok/chat/dao/MessageDao.java
+++ src/main/java/com/helperok/chat/dao/MessageDao.java
@@ -1,7 +1,6 @@
 package com.helperok.chat.dao;
 
 import com.helperok.chat.entity.Message;
-import com.helperok.chat.entity.Session;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -10,11 +9,11 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import java.util.Date;
 import java.util.List;
 
-public interface MessageDao extends PagingAndSortingRepository<Message,Long>{
+public interface MessageDao extends PagingAndSortingRepository<Message, Long> {
     @Query("select m from Message m where ((m.senderId=?1 and m.receiverId =?2) or (m.senderId=?2 and m.receiverId=?1)) and m.createTime>?3 order by m.createTime desc")
     List<Message> findHistoryByIds(long userId, long otherId, Date max_ts, Pageable page);
 
-    @Query(value = "select m.* from chat_message m where (m.sender_id=?1 and m.receiver_id =?2) or (m.sender_id=?2 and m.receiver_id=?1) order by m.create_time desc LIMIT 1",nativeQuery = true)
+    @Query(value = "SELECT m.* FROM chat_message m WHERE (m.sender_id=?1 AND m.receiver_id =?2) OR (m.sender_id=?2 AND m.receiver_id=?1) ORDER BY m.create_time DESC LIMIT 1", nativeQuery = true)
     Message findTopMessage(Long userId, Long otherId);
 
     @Query("select m from  Message m where (m.senderId=?1 or m.receiverId=?1) and m.createTime>?2 order by m.createTime desc")
