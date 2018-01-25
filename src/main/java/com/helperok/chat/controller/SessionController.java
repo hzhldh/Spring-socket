476311da932968464560c67b8815d9996e245414
HuangZhiHao <498641239@qq.com>
2018-01-25 16:12:18 +0800

    会话列表接口完善

--- src/main/java/com/helperok/chat/controller/SessionController.java
+++ src/main/java/com/helperok/chat/controller/SessionController.java
@@ -1,13 +1,18 @@
 package com.helperok.chat.controller;
 
+import com.helperok.chat.entity.Message;
 import com.helperok.chat.entity.Session;
+import com.helperok.chat.service.MessageService;
 import com.helperok.chat.service.SessionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/session")
@@ -15,14 +20,27 @@ public class SessionController {
     @Autowired
     private SessionService sessionService;
 
-    @RequestMapping(value = "/recent", method = RequestMethod.GET)
-    public List<Session> findSessions(long userId) {
+    @Autowired
+    private MessageService messageService;
 
-        //循环查询最后一条消息
+    @RequestMapping(value = "/recent", method = RequestMethod.GET)
+    public List<Object> findSessions(long userId) {
+        List<Session> sessionList=sessionService.findSessionByUserId(userId);
 
         //查询未读消息数
+        List<Object> sessions = new ArrayList<>();
+        for (Session session : sessionList) {
+            Map<String, Object> item = new HashMap<>();
+            item.put("session", session);
+            //查询最后一条消息
+            Message msg = messageService.findTopMessage(session.getSenderId(), session.getReceiverId());
+            item.put("lastMsg", msg);
+            //查询未读数
+            item.put("unRead", messageService.findUnReadNum(session.getSenderId().equals(userId) ? session.getReceiverId() : session.getSenderId(), userId));
+            sessions.add(item);
+        }
 
-        return sessionService.findSessionByUserId(userId);
+        return sessions;
     }
 
     @RequestMapping(value = "/delete", method = RequestMethod.POST)
--- src/main/java/com/helperok/chat/dao/MessageDao.java
+++ src/main/java/com/helperok/chat/dao/MessageDao.java
@@ -14,7 +14,7 @@ public interface MessageDao extends PagingAndSortingRepository<Message,Long>{
     @Query("select m from Message m where ((m.senderId=?1 and m.receiverId =?2) or (m.senderId=?2 and m.receiverId=?1)) and m.createTime>?3 order by m.createTime desc")
     List<Message> findHistoryByIds(long userId, long otherId, Date max_ts, Pageable page);
 
-    @Query(value = "select m from Message m where (m.senderId=?1 and m.receiverId =?2) or (m.senderId=?2 and m.receiverId=?1) order by m.createTime desc LIMIT 1",nativeQuery = true)
+    @Query(value = "select m.* from chat_message m where (m.sender_id=?1 and m.receiver_id =?2) or (m.sender_id=?2 and m.receiver_id=?1) order by m.create_time desc LIMIT 1",nativeQuery = true)
     Message findTopMessage(Long userId, Long otherId);
 
     @Query("select m from  Message m where (m.senderId=?1 or m.receiverId=?1) and m.createTime>?2 order by m.createTime desc")
